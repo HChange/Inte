331ba8dded177ee92e6f9ccb9b8b7fd8c41c0f9c
change <2583608715@qq.com>
2020-04-26 15:20:11 +0800

    降低react native版本

--- pages/upload/Upload.tsx
+++ pages/upload/Upload.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {View, Text} from 'react-native';
 import {NavigationProp} from '@react-navigation/native';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
-
+import { Button } from '@ant-design/react-native';
+import ImagePicker from 'react-native-image-picker'
 const Tab = createMaterialTopTabNavigator();
 type Props = {
   navigation: NavigationProp<any>;
@@ -15,9 +16,41 @@ function Home() {
   );
 }
 function Set() {
+  const options = {
+    title: 'Select Avatar',
+    customButtons: [{name: 'fb', title: 'Choose Photo from Facebook'}],
+    storageOptions: {
+      skipBackup: true,
+      path: 'images',
+    },
+  };
+  const [image, setImage] = useState<any>()
+  function openChoose(){
+    ImagePicker.showImagePicker(options, response => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      } else {
+        const source = {uri: response.uri};
+
+        // You can also display the image using data:
+        // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+        setImage(source);
+      }
+    });
+  }
   return (
     <View>
       <Text>set</Text>
+      <Button onPress={()=>{
+        openChoose();
+      }}>选择图片</Button>
     </View>
   );
 }
